e2a7a3a0de5d42547b470bdff06fefd0b775e578
chhx4567 <chhx4567@163.com>
2014-11-29 23:13:08 +0800

    第一次提交

---
+++ src/part1/FirstSample.java
@@ -0,0 +1,10 @@
+package part1;
+
+/**
+ * Created by vilen on 2014/11/29.
+ */
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello,World!'");
+    }
+}
